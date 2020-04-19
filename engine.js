0654a29dc3afc908a0768c48d0eccda9f503775c
Cristian Díaz <cristiandiaztk@gmail.com>
2020-04-19 13:09:04 +0100

    Update engine.js

--- engine.js
+++ engine.js
@@ -1,6 +1,6 @@
 var ca_texto = "Este sitio web utiliza cookies propias y de terceros para realizar estadísticas relacionadas con la afluencia de tráfico del mismo. Si no está de acuerdo, por favor, abandone el sitio web.";
 var ca_urlpolitica="/";
-var ca_estilosdiv='display: block;position: fixed;bottom: 0;left: 0;right: 0;text-align: center;padding: 15px 30px;background-color: rgba(255,255,255,0.9);border: 1px solid #BDBDBD;';
+var ca_estilosdiv='display: block;position: fixed;bottom: 0;left: 0;right: 0;text-align: center;padding: 15px 30px;background-color: rgba(255,255,255,0.9);border: 1px solid #BDBDBD;opacity: 0.9;';
 document.body.innerHTML += "<div class='cookie-advice' style='display:none;'></div>";
 function ca_showadvice(){
     var textopolitica="";
